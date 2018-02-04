f210a8fa7fda0f5ac9d6db20ed3feba1f7b341d0
Harrison Ku <31172836+hmku@users.noreply.github.com>
2018-02-03 21:33:00 -0800

    template for bible data

---
+++ Data.java
@@ -0,0 +1,118 @@
+package com.example.hmku.data;
+
+/**
+ * This class holds the backend data for the MoodBible.
+ */
+
+public class Data {
+
+    /* categories: happy sad angry stressed burntout ashamed jealous insecure */
+
+    /*
+     * Format: REFERENCE | VERSE
+     */
+    public final String[] HAPPY_VERSES = {
+
+    };
+
+    public final String[] SAD_VERSES = {
+
+    };
+
+    public final String[] ANGRY_VERSES = {
+
+    };
+
+    public final String[] STRESSED_VERSES = {
+
+    };
+
+    public final String[] BURNTOUT_VERSES = {
+
+    };
+
+    public final String[] ASHAMED_VERSES = {
+
+    };
+
+    public final String[] JEALOUS_VERSES = {
+
+    };
+
+    public final String[] INSECURE_VERSES = {
+
+    };
+
+    /*
+     * Format: TITLE | AUTHOR | LINK
+     */
+    public final String[] HAPPY_SONGS = {
+
+    };
+
+    public final String[] SAD_SONGS = {
+
+    };
+
+    public final String[] ANGRY_SONGS = {
+
+    };
+
+    public final String[] STRESSED_SONGS = {
+
+    };
+
+    public final String[] BURNTOUT_SONGS = {
+
+    };
+
+    public final String[] ASHAMED_SONGS = {
+
+    };
+
+    public final String[] JEALOUS_SONGS = {
+
+    };
+
+    public final String[] INSECURE_SONGS = {
+
+    };
+
+    /*
+     * Format: DESCRIPTION | LINK
+     */
+
+    public final String[] HAPPY_TESTIMONIES = {
+
+    };
+
+    public final String[] SAD_TESTIMONIES = {
+
+    };
+
+    public final String[] ANGRY_TESTIMONIES = {
+
+    };
+
+    public final String[] STRESSED_TESTIMONIES = {
+
+    };
+
+    public final String[] BURNTOUT_TESTIMONIES = {
+
+    };
+
+    public final String[] ASHAMED_TESTIMONIES = {
+
+    };
+
+    public final String[] JEALOUS_TESTIMONIES = {
+
+    };
+
+    public final String[] INSECURE_TESTIMONIES = {
+
+    };
+
+
+}
